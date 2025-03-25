8d0c3a5888b98273e6e1a0c23406ac8df951a847
EudesGomes017 <eudesgomes017@gmail.com>
2025-03-24 21:24:00 -0300

    Feat: Create Project

---
+++ src/main/java/br/com/LevelNetWork/Payment/Pix/Application.java
@@ -0,0 +1,13 @@
+package br.com.LevelNetWork.Payment.Pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
